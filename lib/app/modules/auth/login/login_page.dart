3d5da17f2ab56beed932dc0e7ae5fda85f77f02e
ElcioLS <elciols@gmail.com>
2022-09-04 15:18:39 -0300

    Layout Tela de Login

--- lib/app/modules/auth/login/login_page.dart
+++ lib/app/modules/auth/login/login_page.dart
@@ -1,69 +1,75 @@
+import 'package:adf_cuidapet/app/core/ui/extensions/size_screen_extension.dart';
+import 'package:adf_cuidapet/app/core/ui/extensions/theme_extension.dart';
 import 'package:adf_cuidapet/app/core/ui/icons/cuidapeticons_icons.dart';
 import 'package:adf_cuidapet/app/core/ui/widgets/cuidapet_default_button.dart';
 import 'package:adf_cuidapet/app/core/ui/widgets/cuidapet_text_form_field.dart';
 import 'package:adf_cuidapet/app/core/ui/widgets/rounded_button_with_icon.dart';
 import 'package:flutter/material.dart';
 
-class LoginPage extends StatelessWidget {
-  final testeEC = TextEditingController();
-  final formKey = GlobalKey<FormState>();
-  LoginPage({Key? key}) : super(key: key);
+part 'widgets/login_form.dart';
+part 'widgets/login_register_buttons.dart';
 
+class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Form(
-          key: formKey,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
           child: Column(
             children: [
-              CuidapetTextFormField(
-                label: 'Login',
-                obscureText: false,
-                controller: testeEC,
-                validator: (String? value) {
-                  if (value == null || value.isEmpty) {
-                    return 'Valor obrigatório';
-                  }
-                  return null;
-                },
+              SizedBox(height: 50.h),
+              Center(
+                child: Image.asset(
+                  'assets/images/logo.png',
+                  width: 162.w,
+                  fit: BoxFit.fill,
+                ),
               ),
-              Text(testeEC.text),
-              ElevatedButton(
-                onPressed: () {
-                  formKey.currentState?.validate();
-                  print(testeEC.text);
-                },
-                child: const Text('Validar'),
-              ),
-              const Icon(Cuidapeticons.facebook),
-              const Icon(Cuidapeticons.google),
-              RoundedButtonWithIcon(
-                onTap: () {},
-                width: 200,
-                color: Colors.indigo,
-                icon: Cuidapeticons.facebook,
-                label: 'facebook',
-              ),
-              RoundedButtonWithIcon(
-                onTap: () {},
-                width: 200,
-                color: Colors.orange,
-                icon: Cuidapeticons.google,
-                label: 'google',
-              ),
-              CuidapetDefaultButton(
-                onPressed: () {},
-                label: 'Entrar',
-              ),
+              const SizedBox(height: 20),
+              const _LoginForm(),
+              const SizedBox(height: 8),
+              const _OrSeparator(),
+              const SizedBox(height: 8),
+              const _LoginRegisterButtons(),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class _OrSeparator extends StatelessWidget {
+  const _OrSeparator();
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Divider(
+            thickness: 1,
+            color: context.primaryColor,
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            'OU',
+            style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.sp,
+                color: context.primaryColor),
+          ),
+        ),
+        Expanded(
+          child: Divider(
+            thickness: 1,
+            color: context.primaryColor,
+          ),
+        ),
+      ],
     );
   }
 }
