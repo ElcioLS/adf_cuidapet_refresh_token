3b93ea1d7b304f123305a14fff05908dc64d616b
ElcioLS <elciols@gmail.com>
2022-09-10 10:05:24 -0300

    Conectando tela de cadastro ao backend e firebase

--- lib/app/modules/auth/register/register_controller.dart
+++ lib/app/modules/auth/register/register_controller.dart
@@ -1,6 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:adf_cuidapet/app/core/exceptions/user_exists_exception.dart';
 import 'package:adf_cuidapet/app/core/logger/app_logger.dart';
 import 'package:adf_cuidapet/app/core/ui/widgets/loader.dart';
+import 'package:adf_cuidapet/app/core/ui/widgets/messages.dart';
 import 'package:adf_cuidapet/app/services/user/user_service.dart';
 import 'package:mobx/mobx.dart';
 
@@ -20,8 +22,17 @@ abstract class _RegisterControllerBase with Store {
 
   Future<void> register(
       {required String email, required String password}) async {
-    Loader.show();
-    await Future.delayed(const Duration(seconds: 2));
-    Loader.hide();
+    try {
+      Loader.show();
+      await _userService.register(email, password);
+      Loader.hide();
+    } on UserExistsException {
+      Loader.hide();
+      Messages.alert('E-mail já utilizado, por favor escolha outro');
+    } catch (e, s) {
+      _log.error('Erro ao registrar usuário', e, s);
+      Loader.hide();
+      Messages.alert('Erro ao registrar usuário');
+    }
   }
 }
